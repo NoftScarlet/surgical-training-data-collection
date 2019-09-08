92733c1c4c73d5bb94f2699201de91af1808429f
Yixiao Chen <ychen239@uottawa.ca>
2019-09-08 16:45:41 -0400

    add TypeScript and TSLint

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,6 @@ const envs = {
   production: 'prod',
 };
 /* eslint-disable global-require,import/no-dynamic-require */
-//const env = envs[process.env.NODE_ENV || 'development'];
-const envConfig = require(`./webpack`);
+const env = envs[process.env.NODE_ENV || 'development'];
+const envConfig = require(`./webpack/webpack.${env}.js`);
 module.exports = webpackMerge(common, envConfig);
