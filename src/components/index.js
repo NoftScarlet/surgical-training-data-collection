5a5638e57f9f1a98e85318bf5771c4a181ace836
unknown <ychen239@uottawa.ca>
2019-09-23 16:07:07 -0400

    create react app + babel, webpack and typescript setup

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Header from 'header/header';
-import Body from 'body/body';
-import Footer from 'footer/footer';
-import AppPage from 'app-page/app-page';
-import ConsentForm from 'forms/consent-form';
-import TestSessionInfo from 'forms/test-session-info';
-
-export { Header, Body, Footer, AppPage, ConsentForm, TestSessionInfo };
