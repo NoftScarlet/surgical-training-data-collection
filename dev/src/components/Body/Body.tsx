b92556fb5f3e51feb424861a197e574f8e60d838
Yixiao Chen <ychen239@uottawa.ca>
2019-09-30 20:08:00 -0400

    update button handler and video section

--- dev/src/components/Body/Body.tsx
+++ dev/src/components/Body/Body.tsx
@@ -1,19 +1,52 @@
 import React from 'react';
 import FormWrapper from '../ModalWrappers/FormWrapper';
+import KnotVid from '../../../public/assets/knot.mp4';
 import { Button } from 'reactstrap';
 
 interface Props {
 }
 
-interface States {
+interface State {
+  videoLink : string
+
 }
 
-class Body extends React.Component <Props, States> {
+class Body extends React.Component <Props, State> {
+
+  state: Readonly<State> = {
+    videoLink: ""
+  };
+
+  clickHandler = (videoName : string) => {
+
+    switch (videoName) {
+
+      case "suturing" :
+        this.setState({videoLink:"http://piano.uottawa.ca/ovl/sites/default/files/video-thumbnails/2017-03/Running%20Subcuticular.mp4"});
+        break;
+
+      case "knot" :
+        this.setState({videoLink: "http://piano.uottawa.ca/ovl/sites/default/files/video-thumbnails/2017-03/How%20To%20Tie%20Surgical%20Knots-%20Two-Handed%20Knot.mp4"});
+        break;
+
+      default: break;
+
+    }
+  };
 
   render() {
     return (
       <div>
-        <FormWrapper />
+        <FormWrapper/>
+        <Button onClick={() =>this.clickHandler("suturing")}>Suturing</Button>
+        <Button onClick={() =>this.clickHandler("knot")}>Knot Tying</Button>
+        <Button>Start Session</Button>
+        <div id={'video-section'}>
+          <video
+            src={this.state.videoLink}
+            className="danmu-video" id="video1" controls={true} width="100%">
+          </video>
+        </div>
       </div>
     );
   }
