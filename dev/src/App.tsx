f2cb445ac734a86d10aebb6713ac5c20c920846a
Yixiao Chen <ychen239@uottawa.ca>
2019-10-03 17:38:13 -0400

    Remove unnecesarry props

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,53 +1,25 @@
-/*import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col, Jumbotron, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Body from './components/Body/Body'
 
-interface Props {
-  toggle: boolean;
-}
+
 
 interface State {
   isOpen: boolean;
 }
 
-class App extends Component<Props, State>{
-  constructor(props: Props){
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = { isOpen: false };
-  }
-  toggle(){
+class App extends Component<{}, State>{
+
+  state: Readonly<State> = {
+    isOpen: false
+  };
+
+  toggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
-  }
+  };
+
+
   render(){
     return(
       <div>
